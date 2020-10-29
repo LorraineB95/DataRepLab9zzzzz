e0328f013d420db7bb9d5af5b0bb5eb6b7ada09f
Lorraine Burke <lburke1001@gmail.com>
2020-10-29 16:58:45 +0000

    lab 4 commi

--- src/components/read.js
+++ src/components/read.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
+export class Read extends React.Component {
 
-export class Read extends React.Component
-{
-
-//state stores data relevent to the component
+    //state stores data relevent to the component
     state = {
 
-        movies: [
-            {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-            "Title": "Charlie Wilson's War",
-            "Year": "2007",
-            "imdbID": "tt0472062",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-            ]
-            
-};
+        movies: []
+    };
+    //componentDidMount executes after the first render on the client side
+    //axios retrives movie information from JSON file in the link
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then((response) => {
+                    this.setState({ movies: response.data.Search })
+                }
+            )
+    //if there is an error an error message will be returned
+            .catch(
+                (error)=>{
+                        console.log(error)
+                }
+            );
+    }
+
     //Render will display the movie data stored in state
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h3>Hello from Read Component</h3>
                 <Movies myMovies={this.state.movies}></Movies>
